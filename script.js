7f918be5a71df03fb97fef8f01b6a6037d4c5d7b
sudhanshu201 <113603064+sudhanshu201@users.noreply.github.com>
2023-01-21 22:14:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 //your code here
 const textbox = document.getElementById("evaluatedText");
+const textbox = document.getElementById("evaluatedText");
 const wordCount = document.getElementById("wordCount");
 
 textbox.addEventListener("input", function() {
     let text = this.value.trim();
+    text = text.replace(/(\r\n|\n|\r)/gm," "); // remove newlines
     text = text.replace(/[^a-zA-Z0-9\s]/g, ""); // remove non-alphanumeric characters
-    wordCount.innerHTML = text.split(/\s+/).length;
+    text = text.replace(/\s+/g,' '); // remove multiple spaces
+    wordCount.innerHTML = text.split(' ').filter(function(v){return v!==''}).length;
 });
